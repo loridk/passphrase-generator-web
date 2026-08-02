64053bb786dcc82d3c4323063c55e2268dc9cb9c
Lori Dunford <loridk@gmail.com>
2026-08-01 22:00:51 -0400

    focus outline on summary

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.scss";
 import App from "./App.jsx";
 
+if (globalThis.location.protocol === "chrome-extension:") {
+  document.documentElement.dataset.appContext = "extension";
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
